d7bee4cc73a9a2f734dc3938f15385493b5f8606
afernandez <afernandez@rocketlawyer.com>
2024-07-07 03:39:13 -0400

    Adding other adapters just in case

--- src/main/java/ai/smartassets/challenge/adapter/out/persistence/CampaignRepositoryAdapter.java
+++ src/main/java/ai/smartassets/challenge/adapter/out/persistence/CampaignRepositoryAdapter.java
@@ -1,4 +1,45 @@
 package ai.smartassets.challenge.adapter.out.persistence;
 
-public class CampaignRepositoryAdapter {
+import ai.smartassets.challenge.aplication.port.out.CampaignRepository;
+import ai.smartassets.challenge.aplication.port.out.CampaignRepositoryPort;
+import ai.smartassets.challenge.domain.Campaign;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public class CampaignRepositoryAdapter implements CampaignRepositoryPort {
+
+    private final CampaignRepository campaignRepository;
+
+    @Autowired
+    public CampaignRepositoryAdapter(CampaignRepository campaignRepository) {
+        this.campaignRepository = campaignRepository;
+    }
+
+    @Override
+    public Page<Campaign> findAll(Pageable pageable) {
+        return campaignRepository.findAll(pageable);
+    }
+
+    @Override
+    public Campaign save(Campaign campaign) {
+        return campaignRepository.save(campaign);
+    }
+
+    @Override
+    public Optional<Campaign> findById(String id) {
+        return campaignRepository.findById(id);
+    }
+
+    @Override
+    public void delete(Campaign campaign) {
+        campaignRepository.delete(campaign);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        campaignRepository.deleteById(id);
+    }
 }
